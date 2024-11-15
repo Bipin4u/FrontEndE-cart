84b1971bfa45343ecbb1299c15ed45217fd89d95
Bipin4u <bipinkumar9739340018@gmail.com>
2024-11-15 14:42:57 +0530

    Added Footer

---
+++ src/component/common/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import "../../../App.css"
+
+const Footer = () => {
+  return (
+    <div className='primaryBackground p-3' style={{textAlign:"center"}}>
+        <h3 className="p-0 m-0">Bipin@2024</h3>
+    </div>
+  )
+}
+
+export default Footer
