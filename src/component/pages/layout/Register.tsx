0c6ec8e80d2293ca085f246088379d8020d3d0fd
bipin4u <bipnkumar9739340018@gmail.com>
2025-02-24 20:02:07 +0530

    Enhanced user Experience

--- src/component/pages/layout/Register.tsx
+++ src/component/pages/layout/Register.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 import "../../../App.css";
 import { useNavigate } from "react-router-dom";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import data from "../../../../data.json";
+import { Toast } from "primereact/toast";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -12,6 +13,8 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const [error, setError] = useState("");
+  const toast = useRef<Toast>(null);
+  const [loader, setLoader] = useState(false);
 
   const validatePassword = (value: string) => {
     setPassword(value);
@@ -28,11 +31,20 @@ const Register = () => {
     }
   };
 
+  const showWarn = (error: string) => {
+    toast.current?.show({
+      severity: "error",
+      summary: "Error",
+      detail: error,
+      life: 3000,
+    });
+  };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (error) {
       return;
     }
+    setLoader(true);
     try {
       const response = await axios.post(`${data.url}/auth/users/`, {
         username: username,
@@ -41,7 +53,10 @@ const Register = () => {
       });
       console.log("Registration successful:", response.data);
       navigate("/login");
+      setLoader(false);
     } catch (error: any) {
+      setLoader(false);
+      showWarn(error.response.data.username);
       console.error(
         "Registration error:",
         error.response ? error.response.data : error.message
@@ -54,9 +69,10 @@ const Register = () => {
       style={{ height: "85vh" }}
       className="d-flex align-items-center justify-content-center"
     >
+      <Toast ref={toast} />
       <div className=" text-center ">
         <div className="h3 mb-4">Please login to your account</div>
-        <form className="Login-form" onSubmit={handleSubmit}>
+        <form className="Login-form " onSubmit={handleSubmit}>
           <div className="m-4">
             <div className="p-inputgroup">
               <span className="p-inputgroup-addon w-25">
@@ -104,11 +120,20 @@ const Register = () => {
                 className={`p-inputtext ${error ? "border-red-500" : ""}`}
               />
             </div>
-            {error && <p className="mt-2 text-red-500 text-sm">{error}</p>}
+            <div>
+              {error && (
+                <p className="mt-2  text-danger text-red-500 text-sm">
+                  {error}
+                </p>
+              )}
+            </div>
           </div>
 
           <div className="d-grid gap-2 m-3 mt-5">
-            <Button label="Submit" className=" primaryBackground p-2 rounded" />
+            <Button
+              label={loader ? "wait..." : "Submit"}
+              className=" primaryBackground p-2 rounded"
+            />
           </div>
         </form>
 
@@ -128,68 +153,3 @@ const Register = () => {
 
 export default Register;
 
-{
-  /* <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100 py-5">
-  <div className="h3 mb-4 text-center">Please Create your account</div>
-  <form className="w-50" onSubmit={handleSubmit}>
-    <div className="m-4">
-      <div className="input-group">
-        <span className="input-group-text w-25">Username</span>
-        <InputText
-          id="username"
-          placeholder="Username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-    </div>
-
-    <div className="m-4">
-     
-      <div className="input-group">
-        <span className="input-group-text w-25">Email</span>
-        <InputText
-          id="email"
-          placeholder="Email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-    </div>
-
-    <div className="m-4">
-      <div className="input-group">
-        <span className="input-group-text w-25">Password</span>
-        <InputText
-          id="password"
-          placeholder="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="form-control"
-          required
-        />
-      </div>
-    </div>
-
-    <div className="d-grid gap-2 mb-4">
-      <Button label="Create" className="primaryBackground p-2" />
-    </div>
-  </form>
-
-  <div className="d-flex justify-content-center mt-4">
-    <div className="m-2">Already have an account?</div>
-    <Button
-      onClick={() => navigate("/login")}
-      label="Login"
-      outlined
-    />
-  </div>
-</div> */
-}
